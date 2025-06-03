ea8118717430c6e991fec8cbdf5c276d3432f429
Ruhanpaco <theoneoddout@gmail.com>
2025-06-03 16:44:55 +0200

    Add Vercel Analytics to track site usage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -40,7 +41,7 @@ export const metadata: Metadata = {
     canonical: "/",
   },
   openGraph: {
-    title: "Free WHOIS Lookup Tool | Domain Information & Registration Details",
+    title: "WHOIS Lookup Tool | Domain Information & Registration Details",
     description: "Find website ownership details with our free WHOIS lookup tool. Get comprehensive domain registration information, DNS records, SSL certificates & more.",
     url: "https://domaininfo.ruhanpacolli.online",
     siteName: "DomainInfo Tool",
@@ -89,6 +90,7 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
